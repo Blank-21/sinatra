dd55c90a7d4798aebe12ec0e364cb25325945783
rayhan <rayhanm@hotmail.co.uk>
2021-01-06 14:37:52 +0000

    keeping_views_clean

--- sinatra/myapp.rb
+++ sinatra/myapp.rb
@@ -11,14 +11,6 @@ get '/secret' do
 end
 
 get'/photo' do
-<<<<<<< HEAD
   @names = ["Amigo", "Oscar", "Viking"].sample
   erb(:index)
-=======
-  "<div style='border: 3px dashed red'>
-  <img src='https://media.wired.com/photos/5be4cd03db23f3775e466767/master/w_2560%2Cc_limit/books-521812297.jpg'>
-  </div>"
->>>>>>> 476d890e7d193d4a8a1c1a0f80cecd36469145ef
 end
-
-
